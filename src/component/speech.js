f174af5508c711c405d1202dbcd0da7d288d9ee7
Babajide Joshua Oke <joshuaoke2000@gmail.com>
2022-12-22 15:16:16 +0000

    File to speech. Hard coded file directiory

--- src/component/speech.js
+++ src/component/speech.js
@@ -1,24 +1,46 @@
 import { useEffect, useState } from 'react'
 import { useSpeechSynthesis } from 'react-speech-kit'
 import useStateRef from 'react-usestateref';
+import random from '../texts/Random.txt'
+import random2 from '../texts/Random2.txt'
 import './SpeechStyle.css'
 function Speech() {
     const [inputtText, setInputText, inputtTextRef] = useStateRef("")
-    const {speak} = useSpeechSynthesis();
+    const { speak } = useSpeechSynthesis();
 
 
+
+    const read = (text) => {
+        fetch(text)
+            .then(res => res.text())
+            .then(res => speak({text : res}));
+    }
 
     return (
         <div className="center">
-            <textarea 
-            rows={10}
-            cols={100}
-            onChange ={(e) =>
-                setInputText(e.target.value)
-            }
-            >
-            </textarea>
-            <button onClick={() => speak({text: inputtTextRef.current})}>Read Text</button>
+            <div>
+                <textarea
+                    rows={10}
+                    cols={100}
+                    onChange={(e) =>
+                        setInputText(e.target.value)
+                    }
+                >
+                </textarea>
+            </div>
+            <div>
+                <button onClick={() => speak({ text: inputtTextRef.current })}>Read Text</button>
+            </div>
+            <div>
+                <button onClick={()=>read(random2)}>Read 2nd Random text file</button>
+            </div>
+
+            <div>
+                <button onClick={() => read(random)}>Read Text 1 from file</button>
+           </div>
+           <div>
+            {/* <input type={'file'} /> */}
+           </div>
         </div>
     )
 }
